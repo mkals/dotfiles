8a0284e04127490c997797e4fa644dd38493f212
Morten Kals <mKals@Mortens-MacBook-Pro.local>
2016-09-02 11:08:36 -0700

    adde gitconfig and vim folder to setup

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -8,7 +8,7 @@
 
 dir=~/Developer/dotfiles                            # dotfiles directory
 olddir=~/Developer/dotfiles/dotfiles_old            # old dotfiles backup directory
-files="vimrc"    # list of files/folders to symlink in homedir
+files="vimrc vim gitconfig"    # list of files/folders to symlink in homedir
 
 ##########
 
